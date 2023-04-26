eedcb03789fbee2efb51a5414abe2e830019d402
Anant Parmar <anant@infibs.com>
2023-04-26 17:12:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Embeddable Web Chat Widget
+
+
+### Built with ChatGPT (GPT-4)
+
+About 95% of the code for this widget is written by GPT-4, I just edited and adjust a few things
+
+###  No external dependencies, pure javascript
+
+A simple and responsive chat widget built with HTML, CSS, and JavaScript. The chat widget is styled using the popular CSS framework, Tailwind CSS (v2), and can be easily integrated into any website with a simple script tag.
+
+
+
+## Features
+
+- Responsive design
+- Smooth animations and transitions
+- Easily customizable
+- Lightweight and dependency-free
+
+## TODO
+
+- Replace Tailwind with a custom CSS file
+- Session Persistance
+- Ajax mechanism to send and receive messages
+
+## Installation
+
+To install the chat widget, follow these steps:
+
+1. Copy the `chat-widget.js` file into your project directory.
+
+2. Add the following script tag to the `<head>` section of your HTML document:
+
+```
+<script async src="./chat-widget.js"></script>
+```
+
+## Usage & Customization
+
+- The code is quite straight forward and easy to follow, you can easily modify it to suit your needs.
+
+- Messages are passed to the `onUserRequest` function, where you can handle user requests and provide appropriate replies. Use the `reply` function to display responses in the chat popup.
+
+- For visual customization, you can directly make changes to the css or you can also replace the Tailwind CSS classes with your own custom CSS classes or inline styles.
+
+## Demo
+
+Here's a live demo of the chat widget:
+
+[Chat Widget Demo](https://anantrp.github.io/chat-widget)
+
+## Contributing
+
+Contributions are welcome! If you find a bug, have a feature request, or want to improve the chat widget, please feel free to open an issue or create a pull request.
+
+## License
+
+This project is open-source and available under the [MIT License](https://choosealicense.com/licenses/mit/).
---
+++ chat-widget.js
@@ -0,0 +1,152 @@
+(function() {
+  document.head.insertAdjacentHTML('beforeend', '<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.16/tailwind.min.css" rel="stylesheet">');
+
+  // Inject the CSS
+  const style = document.createElement('style');
+  style.innerHTML = `
+  .hidden {
+    display: none;
+  }
+  #chat-widget-container {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    flex-direction: column;
+  }
+  #chat-popup {
+    height: 70vh;
+    max-height: 70vh;
+    transition: all 0.3s;
+    overflow: hidden;
+  }
+  @media (max-width: 768px) {
+    #chat-popup {
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      max-height: 100%;
+      border-radius: 0;
+    }
+  }
+  `;
+
+  document.head.appendChild(style);
+
+  // Create chat widget container
+  const chatWidgetContainer = document.createElement('div');
+  chatWidgetContainer.id = 'chat-widget-container';
+  document.body.appendChild(chatWidgetContainer);
+  
+  // Inject the HTML
+  chatWidgetContainer.innerHTML = `
+    <div id="chat-bubble" class="w-16 h-16 bg-gray-800 rounded-full flex items-center justify-center cursor-pointer text-3xl">
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+      </svg>
+    </div>
+    <div id="chat-popup" class="hidden absolute bottom-20 right-0 w-96 bg-white rounded-md shadow-md flex flex-col transition-all text-sm">
+      <div id="chat-header" class="flex justify-between items-center p-4 bg-gray-800 text-white rounded-t-md">
+        <h3 class="m-0 text-lg">Chat Widget by GPT4</h3>
+        <button id="close-popup" class="bg-transparent border-none text-white cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div id="chat-messages" class="flex-1 p-4 overflow-y-auto"></div>
+      <div id="chat-input-container" class="p-4 border-t border-gray-200">
+        <div class="flex space-x-4 items-center">
+          <input type="text" id="chat-input" class="flex-1 border border-gray-300 rounded-md px-4 py-2 outline-none w-3/4" placeholder="Type your message...">
+          <button id="chat-submit" class="bg-gray-800 text-white rounded-md px-4 py-2 cursor-pointer">Send</button>
+        </div>
+        <div class="flex text-center text-xs pt-4">
+          <span class="flex-1">Prompted by <a href="https://twitter.com/anantrp" target="_blank" class="text-indigo-600">@anantrp</a></span>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Add event listeners
+  const chatInput = document.getElementById('chat-input');
+  const chatSubmit = document.getElementById('chat-submit');
+  const chatMessages = document.getElementById('chat-messages');
+  const chatBubble = document.getElementById('chat-bubble');
+  const chatPopup = document.getElementById('chat-popup');
+  const closePopup = document.getElementById('close-popup');
+
+  chatSubmit.addEventListener('click', function() {
+    
+    const message = chatInput.value.trim();
+    if (!message) return;
+    
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
+    chatInput.value = '';
+
+    onUserRequest(message);
+
+  });
+
+  chatInput.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+      chatSubmit.click();
+    }
+  });
+
+  chatBubble.addEventListener('click', function() {
+    togglePopup();
+  });
+
+  closePopup.addEventListener('click', function() {
+    togglePopup();
+  });
+
+  function togglePopup() {
+    const chatPopup = document.getElementById('chat-popup');
+    chatPopup.classList.toggle('hidden');
+    if (!chatPopup.classList.contains('hidden')) {
+      document.getElementById('chat-input').focus();
+    }
+  }  
+
+  function onUserRequest(message) {
+    // Handle user request here
+    console.log('User request:', message);
+  
+    // Display user message
+    const messageElement = document.createElement('div');
+    messageElement.className = 'flex justify-end mb-3';
+    messageElement.innerHTML = `
+      <div class="bg-gray-800 text-white rounded-lg py-2 px-4 max-w-[70%]">
+        ${message}
+      </div>
+    `;
+    chatMessages.appendChild(messageElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  
+    chatInput.value = '';
+  
+    // Reply to the user
+    setTimeout(function() {
+      reply('Hello! This is a sample reply.');
+    }, 1000);
+  }
+  
+  function reply(message) {
+    const chatMessages = document.getElementById('chat-messages');
+    const replyElement = document.createElement('div');
+    replyElement.className = 'flex mb-3';
+    replyElement.innerHTML = `
+      <div class="bg-gray-200 text-black rounded-lg py-2 px-4 max-w-[70%]">
+        ${message}
+      </div>
+    `;
+    chatMessages.appendChild(replyElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+  
+})();
